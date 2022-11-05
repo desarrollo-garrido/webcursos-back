0673e43ea5069187a97e7b53c461cd732b8778dc
Antonio Garrido Garrido <desarrollo@garrido.pro>
2022-11-05 20:53:07 +0100

    commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const a = "a";
-console.log(a);
+const express = require("express");
+
+const app = express();
+
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Word a todo");
+});
+
+app.listen(PORT, () => {
+  console.log(`App escuhando en el puerto con numero${PORT}`);
+});
